08923399dda6f4e086c739e6d9c2d76832da65a3
rparra <Rparra@devtop.io>
2023-11-09 14:40:03 -0400

    modal cerrado al enviar el mensaje

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,109 +1,119 @@
+import { useSnackbar } from "notistack";
 import React, { useRef, useState } from "react";
-import { FormattedMessage } from "react-intl";
-import "../styles/scss/_contact.scss";
-import close from "../assets/svg/cerrar.svg";
 import ReCAPTCHA from "react-google-recaptcha";
-import {useSnackbar} from "notistack";
-
+import { FormattedMessage } from "react-intl";
+import close from "../assets/svg/cerrar.svg";
+import "../styles/scss/_contact.scss";
 
 export const Contact = ({ showContact, setShowContact }) => {
-
   const { enqueueSnackbar } = useSnackbar();
 
   const captcha = useRef(null);
 
-    function onChange(value) {
-      captcha.current = value;
-        console.log("Captcha value:", value);
-      }
+  function onChange(value) {
+    captcha.current = value;
+    console.log("Captcha value:", value);
+  }
 
-      const [nombre, setNombre] = useState("");
-      const [correo, setCorreo] = useState("");
-      const [mensaje, setMensaje] = useState("");
-    
-      const [error, setError] = useState(false);
-    
-      const validateEmail = (email) => {
-        const reg =
-          /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)$/;
-        return reg.test(email);
-      };
-    
-      const handleSubmit = (event) => {
-        if (nombre === "" || correo === "" || mensaje === "") {
-          setError(true);
-          enqueueSnackbar("Por favor complete todos los campos", {
-            variant: "error",
-            anchorOrigin: {
-              vertical: "top",
-              horizontal: "right",
-            },
-          });
-          return;
-        }
-        if (!validateEmail(correo)) {
-          setError(true);
-          enqueueSnackbar("Por favor introduzca un correo valido", {
-            variant: "error",
-            anchorOrigin: {
-              vertical: "top",
-              horizontal: "right",
-            },
-          });
-          return;
-        }
-        if (captcha.current === null) {
-          setError(true);
-          enqueueSnackbar("Por favor complete el captcha", {
-            variant: "error",
-            anchorOrigin: {
-              vertical: "top",
-              horizontal: "right",
-            },
-          });
-          return;
-        }
-    
-        setError(false);
-    
-        enqueueSnackbar("mensaje enviado", {
-            variant: "success",
-            anchorOrigin: {
-              vertical: "top",
-              horizontal: "right",
-            },
-          });
-        return {
-          nombre: nombre,
-          correo: correo,
-          mensaje: mensaje,
-          captcha: captcha.current,
-        };
-      };
+  const [nombre, setNombre] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [mensaje, setMensaje] = useState("");
 
+  const [error, setError] = useState(false);
+
+  const validateEmail = (email) => {
+    const reg =
+      /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)$/;
+    return reg.test(email);
+  };
+
+  const handleSubmit = (event) => {
+    if (nombre === "" || correo === "" || mensaje === "") {
+      setError(true);
+      enqueueSnackbar("Por favor complete todos los campos", {
+        variant: "error",
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "right",
+        },
+      });
+      return;
+    }
+    if (!validateEmail(correo)) {
+      setError(true);
+      enqueueSnackbar("Por favor introduzca un correo valido", {
+        variant: "error",
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "right",
+        },
+      });
+      return;
+    }
+    if (captcha.current === null) {
+      setError(true);
+      enqueueSnackbar("Por favor complete el captcha", {
+        variant: "error",
+        anchorOrigin: {
+          vertical: "top",
+          horizontal: "right",
+        },
+      });
+      return;
+    }
+
+    setError(false);
+
+    enqueueSnackbar("mensaje enviado", {
+      variant: "success",
+      anchorOrigin: {
+        vertical: "top",
+        horizontal: "right",
+      },
+    });
+    setShowContact(false);
+    return {
+      nombre: nombre,
+      correo: correo,
+      mensaje: mensaje,
+      captcha: captcha.current,
+    };
+  };
 
   return (
-    <div className={showContact ? "cont-wrapper " : "d-none"} >
-    <div className={showContact ? "inline-block container-contact" : "d-none"} style={{ border: "1px solid #00B8FF", padding: "10px", maxWidth: "50% !important" }}>
-      <div>
-        <h3
-          style={{
-            fontSize: "1.5rem",
-            padding: ".7em",
-            boxShadow: "#ffffff1f 0px 2px 3px 0px",
-            borderBottomLeftRadius: 0,
-            borderBottomRightRadius: 0,
-            
-          }}
-        >
-          <FormattedMessage id="contact.title" defaultMessage="Contact" />
-        </h3>
+    <div className={showContact ? "cont-wrapper " : "d-none"}>
+      <div
+        className={showContact ? "inline-block container-contact" : "d-none"}
+        style={{
+          border: "1px solid #00B8FF",
+          padding: "10px",
+          maxWidth: "50% !important",
+        }}
+      >
+        <div>
+          <h3
+            style={{
+              fontSize: "1.5rem",
+              padding: ".7em",
+              boxShadow: "#ffffff1f 0px 2px 3px 0px",
+              borderBottomLeftRadius: 0,
+              borderBottomRightRadius: 0,
+            }}
+          >
+            <FormattedMessage id="contact.title" defaultMessage="Contact" />
+          </h3>
 
-        <img src={close} className="btn-close-contact" onClick={()=> {setShowContact(false)}}/>
-      </div>
+          <img
+            src={close}
+            className="btn-close-contact"
+            onClick={() => {
+              setShowContact(false);
+            }}
+          />
+        </div>
 
-      <form className="p-2 form-contact my-3">
-      <div className="mb-3">
+        <form className="p-2 form-contact my-3">
+          <div className="mb-3">
             <label htmlFor="projectname" className="form-label left">
               <FormattedMessage id="contact.name" defaultMessage="Name" />
             </label>
@@ -115,10 +125,10 @@ export const Contact = ({ showContact, setShowContact }) => {
               maxLength={100}
               onChange={(e) => setNombre(e.target.value)}
             />
-        </div>
-        <div className="mb-3">
+          </div>
+          <div className="mb-3">
             <label htmlFor="projectdescription" className="form-label">
-              <FormattedMessage id="contact.email" defaultMessage="Email"/>
+              <FormattedMessage id="contact.email" defaultMessage="Email" />
             </label>
             <input
               type="text"
@@ -130,31 +140,36 @@ export const Contact = ({ showContact, setShowContact }) => {
             />
           </div>
           <div>
-          <label htmlFor="capaName" className="form-label">
-                <FormattedMessage id="contact.message" defaultMessage="Message"/>
+            <label htmlFor="capaName" className="form-label">
+              <FormattedMessage id="contact.message" defaultMessage="Message" />
             </label>
-          <textarea 
-          class="form-control"
-           aria-label="With textarea" 
-           className="form-control-sm w-100" 
-           style={{height: "100px"}}
-           onChange={(e) => setMensaje(e.target.value)}
-           ></textarea>
+            <textarea
+              class="form-control"
+              aria-label="With textarea"
+              className="form-control-sm w-100"
+              style={{ height: "100px" }}
+              onChange={(e) => setMensaje(e.target.value)}
+            ></textarea>
           </div>
-      </form>
-        
+        </form>
+
         <div className="d-flex justify-content-center align-items-center">
-        <ReCAPTCHA
-        onChange={onChange}
-        sitekey="6LdToOAoAAAAACCddxc0a_F380wEUzHvJFuM_lSY" />
+          <ReCAPTCHA
+            onChange={onChange}
+            sitekey="6LdToOAoAAAAACCddxc0a_F380wEUzHvJFuM_lSY"
+          />
         </div>
 
         <div className="p-2">
-        <button className="__boton-mediano enphasis-button" style={{width: "200px"}} onClick={handleSubmit}>
-            <FormattedMessage id="contact.send" defaultMessage="Send"/>
-        </button>
+          <button
+            className="__boton-mediano enphasis-button"
+            style={{ width: "200px" }}
+            onClick={handleSubmit}
+          >
+            <FormattedMessage id="contact.send" defaultMessage="Send" />
+          </button>
         </div>
-        </div>
-  </div>
+      </div>
+    </div>
   );
 };
