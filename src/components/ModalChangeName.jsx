ced03c89633991a7d53ee140de253c8c88efa5cc
rparra <Rparra@devtop.io>
2023-11-30 10:41:53 -0400

    cambiado modal para cambiar el nombre de la capa

--- src/components/ModalChangeName.jsx
+++ src/components/ModalChangeName.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { FormattedMessage } from "react-intl"
+import { ReactComponent as Close } from "../assets/svg/close-ellipse.svg"
 import getCapas from "../utils/getCapas"
 import setSelectedCapaId from "../utils/setSelectedCapaId"
 
@@ -15,11 +16,11 @@ export default function ModalChangeName(props) {
     const capaIndex = capas.findIndex((capa) => capa.id === selectedCapa.id)
     capas[capaIndex].name = name
     capas[capaIndex].directory = name
-    setCapita(name)
     localStorage.setItem("capas", JSON.stringify(capas))
     capas = getCapas()
     setSelectedCapaId(capas.find((capa) => capa.id === selectedCapa.id))
     setCapas(capas)
+    setCapita(name)
   }
 
   return (
@@ -34,27 +35,79 @@ export default function ModalChangeName(props) {
       >
         <div
           className="card-modal"
-          style={{ border: "1px solid #00B8FF", padding: "10px" }}
+          style={{
+            border: "1px solid #00B8FF",
+            padding: "10px",
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: "column"
+          }}
         >
-          <form className="form-capa-name" onSubmit={(e) => e.preventDefault()}>
-            <label htmlFor="capaName" className="form-label">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "100%"
+            }}
+          >
+            <label
+              htmlFor="capaName"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                height: "2rem",
+                width: "100%"
+              }}
+            >
               <FormattedMessage
-                id="form.layername"
-                defaultMessage="Layer Name"
+                id="form.changelayername"
+                defaultMessage="Change Layer Name"
               />
             </label>
-            <input
-              type="text"
-              className="form-control-sm w-100"
-              id="capaName"
-              name="capaName"
-              value={capita}
-              onChange={setNewNameCapa}
+            <Close
+              style={{ width: "20px", height: "20px" }}
+              onClick={() => setOpenNewModal(false)}
             />
-          </form>
-          <button onClick={() => setOpenNewModal(false)}>
-            Change Layer name
-          </button>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              justifyContent: "center",
+              alignContent: "center",
+              alignItems: "center",
+              marginTop: "2rem"
+            }}
+          >
+            <div
+              style={{
+                width: "50%",
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column"
+              }}
+            >
+              <input
+                type="text"
+                className="form-control-sm w-100"
+                id="capaName"
+                name="capaName"
+                value={capita.replace(" ", "-")}
+                onChange={setNewNameCapa}
+                style={{ border: "1px solid #00B8FF", marginBottom: "2rem" }}
+              />
+              <button
+                className="__boton-mediano mx-0 color enphasis-button"
+                onClick={() => setOpenNewModal(false)}
+              >
+                <FormattedMessage
+                  id="form.changelayerButton"
+                  defaultMessage="Change"
+                />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
