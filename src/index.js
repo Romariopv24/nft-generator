cb4ec4f21f4ab406691bd8342f9e0e88e503c72c
rparra <Rparra@devtop.io>
2023-12-05 15:52:21 -0400

    pushing

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { LangProvider } from './context/langContext';
-import { SnackbarProvider } from 'notistack'
+import { SnackbarProvider } from "notistack"
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App"
+import { LangProvider } from "./context/langContext"
 
+if (process.env.NODE_ENV === "production") {
+  console.log = () => {}
+}
 
 ReactDOM.render(
   <LangProvider>
     <React.StrictMode>
-    <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </SnackbarProvider>
+      <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SnackbarProvider>
     </React.StrictMode>
   </LangProvider>,
-  document.getElementById('root')
-);
-
+  document.getElementById("root")
+)
