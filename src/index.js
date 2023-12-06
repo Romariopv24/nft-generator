6da0a06805bf4d651514ad5474e6d6da0c2a6c9f
rparra <Rparra@devtop.io>
2023-12-06 11:32:38 -0400

    agregando comentarios positivos al codigo :)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import { LangProvider } from "./context/langContext"
 
+// para que no se vean los logs en production karajo...
 if (process.env.NODE_ENV === "production") {
   console.log = () => {}
 }
--- src/utils/zustand/store.js
+++ src/utils/zustand/store.js
@@ -1,6 +1,7 @@
 import { create } from "zustand"
 
 //Va pues cree un store pa un conio... pero bueno espero que algun dia se use porque este proyecto esta horrible
+// Al final si se uso esta mieldaaa ajajaja
 export const useStoreSignal = create((set) => ({
   signal: false,
   setSignal: () => set((state) => ({ signal: !state.signal }))
