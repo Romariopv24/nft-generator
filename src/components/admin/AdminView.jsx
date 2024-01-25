7d4de12e49b7406b07a54039c5d08708085fcbd9
rparra <Rparra@devtop.io>
2024-01-25 15:49:41 -0400

    agregada otra grafica por si acaso

--- src/components/admin/AdminView.jsx
+++ src/components/admin/AdminView.jsx
@@ -42,7 +42,7 @@ export default function AdminView() {
   return (
     <Stack alignItems={"center"}>
       <Stack my={5} mx={"auto"}>
-        <Graph />
+        <Graph dataAdmin={dataAdmin} />
       </Stack>
       <Stack direction={"row"} mt={1} justifyContent={"center"} width={"80%"}>
         <Table dataAdmin={dataAdmin} />
