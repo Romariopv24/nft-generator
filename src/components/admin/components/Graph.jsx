f4fa1b958f014e52d45ffabfb34afccc934ae6c7
rparra <Rparra@devtop.io>
2024-01-26 13:59:50 -0400

    Cambiado datos, por ahora hardcodeados

--- src/components/admin/components/Graph.jsx
+++ src/components/admin/components/Graph.jsx
@@ -1,11 +1,8 @@
 import { PieChart, pieArcLabelClasses } from "@mui/x-charts/PieChart"
 export default function Graph() {
   const data = [
-    { id: 0, value: 10, label: "series A" },
-    { id: 1, value: 15, label: "series B" },
-    { id: 2, value: 20, label: "series C" },
-    { id: 3, value: 20, label: "series D" },
-    { id: 4, value: 20, label: "series F" }
+    { id: 0, value: 10, label: "Usuarios" },
+    { id: 1, value: 15, label: "Colecciones" }
   ]
   return (
     <PieChart
